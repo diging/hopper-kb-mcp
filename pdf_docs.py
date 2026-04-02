80c9fea98c4accd00e2902e9c3ad041c8678737c
TatsatJha <TatsatTarun@gmail.com>
2026-04-02 11:11:38 -0700

    add the get_pdf function to check if a pdf exists and return the document so that it can be updated, needs fixing

--- pdf_docs.py
+++ pdf_docs.py
@@ -1,20 +1,43 @@
 from unstructured.partition.pdf import partition_pdf
 
-import os, time
+import os
+import time
 
 import documents
+from dbmodel import Document
 
 
-def add_pdf(file: bytes, filename: str, title: str, url: str=None):
-    
+def get_pdf(title: str):
+
+    document_list = documents.get_documents()[1]
+    print(document_list)
+
+    for doc in document_list:
+        print(doc)
+        if doc['title'] == title and doc['doc_type'] == documents.DocumentTypes.PDF.value:
+            return Document(title=doc['title'], url=doc['url'], doc_type=documents.DocumentTypes.PDF.value, created_at=doc['created_at'], modified_at=doc['modified_at'], chunks=doc['chunks'])
+
+    return None
+
+
+def add_pdf(file: bytes, filename: str, title: str, url: str = None):
+
     file_path = _save_file(file, filename)
 
     elements = partition_pdf(filename=f"{file_path}")
+    doc = get_pdf(title)
+
+    if doc:
+        print("updating")
+        return documents.update_document(doc, elements, title, documents.DocumentTypes.PDF.value, url)
+
+    print("adding")
     return documents.add_document(elements, title, documents.DocumentTypes.PDF.value, url)
+
 
 def _save_file(file: bytes, filename: str) -> str:
     """Helper to save an uploaded file to disk and return the file path."""
-    
+
     # for whatever reason, makedirs refused to create the subfolder,
     # so two steps it is.
     data_path = f"{os.environ.get("DATA_DIR", "./data")}"
